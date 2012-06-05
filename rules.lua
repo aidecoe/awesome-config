d5bcd8e01297f61b31d626b19b3f9d5dd1c5b3cd
Amadeusz Żołnowski <aidecoe@aidecoe.name>
2012-06-05 15:54:58 +0200

    Move Rules to separate file.
    Rewritten rules from my dwm config with few exceptions.

---
+++ rules.lua
@@ -0,0 +1,28 @@
+-- {{{ Rules
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons } },
+    { rule = { class = "pinentry" },
+      properties = { floating = true } },
+    { rule = { class = "MPlayer" },
+      properties = { floating = true } },
+    { rule = { class = "Gimp" },
+      properties = { tag = tags[1][7],
+                     floating = true } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][8] } },
+    { rule = { class = "Navigator" },
+      properties = { floating = false } },
+    { rule = { class = "Opera" },
+      properties = { tag = tags[1][8],
+                     floating = false } },
+    { rule = { instance = "xmpp-irc-email" },
+      properties = { tag = tags[1][9],
+                     floating = false } },
+}
+-- }}}
